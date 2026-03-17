9a0e921b0506d432d7ea8d2f014e0819e9944156
Juan Camilo Quevedo Jaimes <juanqueja64@gmail.com>
2026-03-17 14:35:45 -0500

    Modificando archivo desde nueva rama

--- Ejercicios_Python.py
+++ Ejercicios_Python.py
@@ -5,3 +5,10 @@ print("Aca estaran los 35 ejercicios propuestos")
     git commit -m "text"
     git push origin main
 """
+
+"""
+    Mas comandos:
+    git branch      --> Muestra la rama
+    git switch -c "nombre de la rama"  --> Crea una nueva rama y se cambia a ella
+    git switch "nombre de la rama"  --> Cambia a la rama indicada
+"""
