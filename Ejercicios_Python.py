625353a871daebadf86d360045cf3eab0a0274ed
Juan Camilo Quevedo Jaimes <juanqueja64@gmail.com>
2026-03-17 14:47:24 -0500

    Añadiendo mas comandos para probar Git

--- Ejercicios_Python.py
+++ Ejercicios_Python.py
@@ -12,3 +12,8 @@ print("Aca estaran los 35 ejercicios propuestos")
     git switch -c "nombre de la rama"  --> Crea una nueva rama y se cambia a ella
     git switch "nombre de la rama"  --> Cambia a la rama indicada
 """
+
+"""
+    git merge "nombre de la rama"  --> Fusiona la rama indicada con la rama actual, debe hacerse desde la rama principal.
+    git push --> Sube los cambios al repositorio remoto
+"""
